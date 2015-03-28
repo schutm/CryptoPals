e722e06cc259c4dc9aa721565c6abd47ea1fcfad
Martin Schut <github@wommm.nl>
2015-03-28 22:20:46 +0100

    Clean-up the invocation to solve the challenges

--- src/challenges/set1.erl
+++ src/challenges/set1.erl
@@ -1,13 +1,24 @@
 -module(set1).
 -author("Martin Schut <martin-github@wommm.nl>").
 
--export([all/0, challenge/1]).
+-export([
+  all/0,
+  convert_hex_to_base64/0,
+  fixed_xor/0,
+  single_byte_xor/0,
+  detect_single_character_xor/0,
+  repeating_key_xor/0]).
 
-all() -> lists:seq(1,5).
+all() ->
+  [
+    convert_hex_to_base64,
+    {"Fixed XOR", fixed_xor},
+    {"Single-byte XOR cipher", single_byte_xor},
+    {"Detect single-character XOR", detect_single_character_xor},
+    {"Implement repeating-key XOR", repeating_key_xor}
+  ].
 
-challenge(1) ->
-  io:fwrite("Convert hex to base64~n"),
-
+convert_hex_to_base64() ->
   Input = <<"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d">>,
   Expected = <<"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t">>,
 
@@ -16,11 +27,9 @@ challenge(1) ->
 
   #{input => Input,
     output => Base64,
-    expectation => Expected};
+    expectation => Expected}.
 
-challenge(2) ->
-  io:fwrite("Fixed XOR~n"),
-
+fixed_xor() ->
   Input1 = <<"1c0111001f010100061a024b53535009181c">>,
   Input2 = <<"686974207468652062756c6c277320657965">>,
   Expected = <<"746865206b696420646f6e277420706c6179">>,
@@ -32,11 +41,9 @@ challenge(2) ->
 
   #{input => io_lib:format("~s XOR ~s", [Input1, Input2]),
     output => HexString,
-    expectation => Expected};
+    expectation => Expected}.
 
-challenge(3) ->
-  io:fwrite("Single-byte XOR cipher~n"),
-
+single_byte_xor() ->
   Input = <<"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736">>,
 
   BitString = cryptopals_bitsequence:bitstring_from_hex(Input),
@@ -44,11 +51,9 @@ challenge(3) ->
   {Key, _GoodnessOfFit, PlainText} = Winner,
 
   #{input => Input,
-    output => io_lib:format("key '~p' results in ~p", [Key, PlainText])};
+    output => io_lib:format("key '~p' results in ~p", [Key, PlainText])}.
 
-challenge(4) ->
-  io:fwrite("Detect single-character XOR~n"),
-
+detect_single_character_xor() ->
   InputFile = "./data/4.txt",
 
   {ok, Device} = file:open(InputFile, [read]),
@@ -61,17 +66,15 @@ challenge(4) ->
   file:close(Device),
 
   Min = fun({_, FitA, _, _} = A, {_, FitB, _, _} = _B) when FitA < FitB -> A;
-           (_A, B) -> B
+    (_A, B) -> B
   end,
   Result = lists:foldl(Min, hd(Guesses), tl(Guesses)),
   {Key, _Fit, PlainText, CipherText} = Result,
 
   #{input => io_lib:format("from file '~s'", [InputFile]),
-    output => io_lib:format("key '~p' results in ~p for cipher <<~s>>", [Key, PlainText, CipherText])};
+    output => io_lib:format("key '~p' results in ~p for cipher <<~s>>", [Key, PlainText, CipherText])}.
 
-challenge(5) ->
-  io:fwrite("Implement repeating-key XOR~n"),
-
+repeating_key_xor() ->
   PlainText = <<"Burning 'em, if you ain't quick and nimble", 10, "I go crazy when I hear a cymbal">>,
   Key = <<"ICE">>,
   Expected = <<"0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f">>,
@@ -81,7 +84,4 @@ challenge(5) ->
 
   #{input => io_lib:format("~p XOR ~p", [PlainText, Key]),
     output => HexString,
-    expectation => Expected};
-
-challenge(_) ->
-  true.
+    expectation => Expected}.
--- src/cryptopals.erl
+++ src/cryptopals.erl
@@ -4,62 +4,60 @@
 -define(AVAILABLE_SETS, [set1]).
 
 %% API
--export([expect/2, solve/1]).
+-export([solve/1]).
 
-solve(ListOfChallengesToSolve) when is_list(ListOfChallengesToSolve) ->
-  ChallengesToSolve = lists:filter(fun(Challenge) -> should_solve(Challenge, ListOfChallengesToSolve) end, all_challenges()),
-  solve_challenges(ChallengesToSolve);
+%% Any combination of Set and { Set, Challenge } is allowed. In addition if only 1
+%% set or challenge should be solved no list is required. As special case the atom
+%% all indicates all challenges.
 solve(all) ->
-  solve_challenges(all_challenges());
-solve(Challenge) ->
-  solve([Challenge]).
-
-expect(ExpectedOutput, Output) ->
-  case Output of
-    ExpectedOutput ->
-      ok;
-    _UnexpectedOutput ->
-      io:fwrite("  Expected output: ~s~n", [ExpectedOutput]),
-      failed
-  end.
+  solve(all_challenges());
+solve(Set) when is_atom(Set) ->
+  solve(set_challenges(Set));
+solve([Challenge|RemainingChallenges]) ->
+  solve(Challenge),
+  solve(RemainingChallenges);
+solve([]) ->
+  ok;
+solve({_Set, _Challenge} = Challenge) ->
+  solve_challenge(Challenge).
 
 %%
 %% Internal functions
 %%
-solve_challenges(ListOfChallengesToSolve) ->
-  lists:foreach(fun(Challenge) -> solve_challenge(Challenge) end, ListOfChallengesToSolve).
+solve_challenge({Set, Challenge}) when not is_tuple(Challenge) ->
+  [FirstLetter|Title] = string:join(string:tokens(atom_to_list(Challenge), "_"), " "),
+  solve_challenge({Set, {[string:to_upper(FirstLetter)|Title], Challenge}});
+solve_challenge({Set, {Title, Challenge}}) ->
+  write_title(Set, Title),
 
-solve_challenge({Set, Challenge}) ->
-  SetNumber = list_to_integer(lists:filter(fun(C) -> C >= $0 andalso C =< $9 end, atom_to_list(Set))),
-  io:fwrite("Set ~p, challenge ~p: ", [SetNumber, Challenge]),
-
-  try Set:challenge(Challenge) of
-    Map = #{input := Input, output := Output} ->
-      io:fwrite("  Input: ~s~n", [Input]),
-      io:fwrite("  Output: ~s~n", [Output]),
-      case maps:get(expectation, Map, Output) of
-        Output ->
-          ok;
-        ExpectedOutput ->
-          io:fwrite("  Expected output: ~s~n", [ExpectedOutput]),
-          failed
-      end
+  try Set:Challenge() of
+    Result ->
+      write_input(Result),
+      write_output(Result),
+      maybe_write_expectation(Result)
   catch
     Exception:Reason -> io:fwrite("  Caught '~p' due to ~p~n", [Exception, Reason])
   end.
 
-should_solve(Challenge, ListOfChallengesToSolve) when is_list(ListOfChallengesToSolve) ->
-  lists:any(fun(ChallengeToSolve) -> should_solve(Challenge, ChallengeToSolve) end, ListOfChallengesToSolve);
-should_solve({SetToSolve, _ChallengeToSolve}, SetToSolve) -> true;
-should_solve({_SetToSolve, ChallengeToSolve}, ChallengeToSolve) -> true;
-should_solve({SetToSolve, ChallengeToSolve}, {SetToSolve, ChallengeToSolve}) -> true;
-should_solve(_, _) -> false.
+write_title(Set, Title) ->
+  SetNumber = lists:filter(fun(C) -> C >= $0 andalso C =< $9 end, atom_to_list(Set)),
+  io:fwrite("Set ~s - challenge '~s'~n", [SetNumber, Title]).
+
+write_input(#{input := Input}) ->
+  io:fwrite("  Input: ~s~n", [Input]).
+
+write_output(#{output := Output}) ->
+  io:fwrite("  Output: ~s~n", [Output]).
+
+maybe_write_expectation(#{output := Output, expectation := ExpectedOutput}) when Output =:= ExpectedOutput ->
+  ok;
+maybe_write_expectation(#{expectation := ExpectedOutput}) ->
+  io:fwrite("  Expected output: ~s~n", [ExpectedOutput]);
+maybe_write_expectation(#{}) ->
+  ok.
 
 all_challenges() ->
-  lists:flatten(lists:map(fun(Set) -> set_challenges(Set) end, ?AVAILABLE_SETS)).
+  lists:flatten([set_challenges(Set) || Set <- ?AVAILABLE_SETS]).
 
 set_challenges(Set) ->
-  lists:map(fun(Challenge) -> create_challenge(Set, Challenge) end, Set:all()).
-
-create_challenge(Set, Challenge) ->
-  {Set, Challenge}.
+  [{Set, Challenge} || Challenge <- Set:all()].
