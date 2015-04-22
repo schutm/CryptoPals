a6946ee44d02aa1110c5badcf9c9c4f91ed9e28d
Martin Schut <github@wommm.nl>
2015-04-23 00:57:42 +0200

    Solve challenge 9: Implement PKCS#7 padding

--- src/challenges/set2.erl
+++ src/challenges/set2.erl
@@ -2,7 +2,20 @@
 -author("Martin Schut <martin-github@wommm.nl>").
 
 -export([
-  all/0]).
+  all/0,
+  implement_pkcs7_padding/0]).
 
 all() ->
-  [].
+  [
+    {"Implement PKCS#7 padding", implement_pkcs7_padding}
+  ].
+
+implement_pkcs7_padding() ->
+  Input = <<"YELLOW SUBMARINE">>,
+  Expected = <<"YELLOW SUBMARINE", 4, 4, 4, 4>>,
+
+  Result = cryptopals_crypto:pkcs7_padding(Input, 20),
+
+  #{input => Input,
+    output => Result,
+    expectation => Expected}.
--- src/cryptopals/cryptopals_crypto.erl
+++ src/cryptopals/cryptopals_crypto.erl
@@ -2,10 +2,16 @@
 -author("Martin Schut <martin-github@wommm.nl").
 
 %% API
--export([block_decrypt/3]).
+-export([block_decrypt/3, pkcs7_padding/2]).
 
 block_decrypt(aes_ecb128, Key, CipherText) ->
   BitSize = bit_size(Key),
   Ivec =  <<0:BitSize>>,
   << <<(crypto:block_decrypt(aes_cbc128, Key, Ivec, CipherBlock))/bitstring>> || <<CipherBlock:BitSize/bitstring>> <= CipherText>>.
 
+pkcs7_padding(BitString, Bytes) ->
+  ByteSize = byte_size(BitString),
+  NumberOfPaddingBytes = Bytes - (ByteSize rem Bytes),
+  PaddingList = cryptopals_utils:for(fun(_) -> <<NumberOfPaddingBytes>> end, 1, NumberOfPaddingBytes),
+  PaddingBitString = list_to_bitstring(PaddingList),
+  <<BitString/bitstring, PaddingBitString/bitstring>>.
